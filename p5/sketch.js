f9fc575edbfabc1ecd92d31505d54f258ffd053a
ericshu1212 <60146887+ericshu1212@users.noreply.github.com>
2020-01-28 11:33:07 -0500

    Add files via upload

---
+++ p5/sketch.js
@@ -0,0 +1,38 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(300);
+
+  //(x, y, width, hight)
+  fill("yellow");
+  rect(250,0,120,150);
+  noFill();
+  rect(0,0,100,50);
+  rect(100,0,150,50);
+  rect(250,0,120,50);
+  rect(0,50,50,100);
+  rect(0,150,50,135);
+  fill("orange");
+  rect(0,285,50,100);
+  fill("black");
+  rect(50,250,100,100);
+    noFill();
+  rect(250,150,60,100);
+  rect(310,150,60,100);
+  rect(250,250,120,50);
+  fill("blue");
+  rect(250,300,120,80);
+  noFill();
+  rect(150,250,100,50);
+  rect(150,300,100,50);
+  fill("black");
+  rect(150,350,100,30);
+  fill("red");
+  rect(50,50,200,200);
+  rect(370,300,20,100);
+  
+  
+
+}
